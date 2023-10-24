abdfa782bb4a337b10cc0df680b47995884c10a9
Francesco Mineo <francymin@gmail.com>
2023-10-24 09:56:13 +0200

    fix(downloader): fixed asset zip download

--- src/Downloader.php
+++ src/Downloader.php
@@ -5,15 +5,13 @@ namespace Ector\ReleaseDownloader;
 class Downloader
 {
     const API_URL = "https://api.github.com/";
+    const API_VERSION = "2022-11-28";
+    const USER_AGENT = "EctorReleaseDownloader";
 
     private $repositoryOwner;
     private $repositoryName;
     private $accessToken;
     private $latestRelease;
-    private $commonHeaders = [
-        "Accept: application/vnd.github+json",
-        "User-Agent: EctorReleaseDownloader",
-    ];
 
     public function __construct($repositoryOwner, $repositoryName, $accessToken = null)
     {
@@ -25,136 +23,18 @@ class Downloader
         $this->repositoryName = $repositoryName;
         $this->accessToken = $accessToken;
 
-        if ($this->accessToken !== null) {
-            $this->commonHeaders[] = "Authorization: Bearer {$this->accessToken}";
-        }
-
         $this->latestRelease = $this->getLatestRelease();
     }
 
-    private function getLatestRelease()
-    {
-        $url = self::API_URL . "repos/{$this->repositoryOwner}/{$this->repositoryName}/releases/latest";
-
-        $options = [
-            'http' => [
-                'header' => implode("\r\n", $this->commonHeaders),
-            ],
-        ];
-
-        $context = stream_context_create($options);
-
-        $releaseInfo = @file_get_contents($url, false, $context);
-
-        if ($releaseInfo) {
-            return json_decode($releaseInfo, true);
-        } else {
-            throw new \Exception("No releases found for the specified repository. Please also check the GitHub Token for private repositories.");
-        }
-    }
-
-    private function getLatestReleaseVersion()
-    {
-        return $this->latestRelease["tag_name"];
-    }
-
-    private function getLatestReleaseAssets()
-    {
-        return $this->latestRelease["assets"][0];
-    }
-
-    private function getLatestReleaseAssetsName()
-    {
-        return $this->getLatestReleaseAssets()["name"];
-    }
-
-    private function getLatestReleaseAssetsDownloadUrl()
-    {
-        return $this->getLatestReleaseAssets()["browser_download_url"];
-        // $assetId = $this->getLatestReleaseAssetsId();
-        // return self::API_URL."repos/{$this->repositoryOwner}/{$this->repositoryName}/releases/assets/{$assetId}";
-    }
-
-    private function downloadFileZip($url)
-    {
-        // $headers = [
-        //     "Accept: application/octet-stream",
-        //     "User-Agent: EctorReleaseDownloader",
-        //     "Authorization: Bearer {$this->accessToken}",
-        // ];
-
-        // $options = [
-        //     'http' => [
-        //         'header' => implode("\r\n", $headers),
-        //     ],
-        // ];
-
-        // $context = stream_context_create($options);
-        // return file_get_contents($url, false, $context);
-
-
-
-
-        // Inizializza una risorsa cURL
-        $ch = curl_init();
-
-        // Imposta l'URL del file ZIP da scaricare
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        // Imposta l'opzione CURLOPT_RETURNTRANSFER per ottenere il contenuto del file
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // Aggiungi user agent
-        $userAgent = 'EctorReleaseDownloader'; // Sostituisci con il tuo User-Agent desiderato
-        curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-    
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/octet-stream'));
-
-
-        // Aggiungi un'intestazione Authorization
-        $authorizationHeader = 'Bearer '.$this->accessToken; // Sostituisci con il token di autorizzazione desiderato
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: ' . $authorizationHeader));
-    
-
-        // Esegue la richiesta cURL
-        $zipFileContents = curl_exec($ch);
-
-        // Verifica se c'è stato un errore durante la richiesta cURL
-        if (curl_errno($ch)) {
-            echo 'Errore durante il download del file ZIP: ' . curl_error($ch);
-            exit;
-        }
-
-        // Chiude la risorsa cURL
-        curl_close($ch);
-        
-        return $zipFileContents;
-    }
-
-    private function downloadFile($url)
-    {
-
-        $options = [
-            'http' => [
-                'header' => implode("\r\n", $this->commonHeaders),
-            ],
-        ];
-
-        $context = stream_context_create($options);
-        return file_get_contents($url, false, $context);
-    }
-
-    private function saveFile($name, $content)
-    {
-        file_put_contents($name, $content);
-    }
-
+    /**
+     * Public Methods
+     */
     public function downloadLatestReleaseAssets()
     {
-        $zipContents = $this->downloadFileZip($this->getLatestReleaseAssetsDownloadUrl());
+        $zipUrl = $this->getLatestReleaseAssetsDownloadUrl();
+        $zipContents = $this->downloadFileZip($zipUrl);
 
         if ($zipContents) {
-            var_dump($zipContents);
             $this->saveFile($this->getLatestReleaseAssetsName(), $zipContents);
             return true;
         } else {
@@ -169,17 +49,108 @@ class Downloader
 
         if ($zipContents) {
             $this->saveFile("latest_release.zip", $zipContents);
-            echo "Zip file downloaded successfully.";
         } else {
             throw new \Exception("Unable to download zip file.");
         }
     }
+
+    public function getLatestReleaseVersion()
+    {
+        return $this->latestRelease["tag_name"];
+    }
+
+
+
+    /**
+     * Private Methods
+     */
+    private function getLatestRelease()
+    {
+        $url = self::API_URL . "repos/{$this->repositoryOwner}/{$this->repositoryName}/releases/latest";
+
+        $options = [
+            'http' => [
+                'header' => implode("\r\n", [
+                    'Accept: vnd.github+json',
+                    'User-Agent: ' . self::USER_AGENT,
+                    'Authorization: Bearer ' . $this->accessToken,
+                    'X-GitHub-Api-Version: ' . self::API_VERSION,
+                ]),
+            ],
+        ];
+
+        $context = stream_context_create($options);
+
+        $releaseInfo = @file_get_contents($url, false, $context);
+
+        if ($releaseInfo) {
+            return json_decode($releaseInfo, true);
+        } else {
+            throw new \Exception("No releases found for the specified repository. Please also check the GitHub Token for private repositories.");
+        }
+    }
+
+    private function getLatestReleaseAssets()
+    {
+        return $this->latestRelease["assets"][0];
+    }
+
+    private function getLatestReleaseAssetsId()
+    {
+        return $this->getLatestReleaseAssets()["id"];
+    }
+
+    private function getLatestReleaseAssetsName()
+    {
+        return $this->getLatestReleaseAssets()["name"];
+    }
+
+    private function getLatestReleaseAssetsDownloadUrl()
+    {
+        // return $this->getLatestReleaseAssets()["browser_download_url"];
+        $assetId = $this->getLatestReleaseAssetsId();
+        return self::API_URL . "repos/{$this->repositoryOwner}/{$this->repositoryName}/releases/assets/{$assetId}";
+    }
+
+    private function downloadFileZip($url)
+    {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/octet-stream',
+            'User-Agent: ' . self::USER_AGENT,
+            'Authorization: Bearer ' . $this->accessToken,
+            'X-GitHub-Api-Version: ' . self::API_VERSION,
+        ]);
+        $zipFileContents = curl_exec($ch);
+        curl_close($ch);
+        return $zipFileContents;
+    }
+
+    private function downloadFile($url)
+    {
+        $options = [
+            'http' => [
+                'header' => implode("\r\n", [
+                    'Accept: vnd.github+json',
+                    'Authorization: Bearer ' . $this->accessToken,
+                    'User-Agent: ' . self::USER_AGENT,
+                    'X-GitHub-Api-Version: ' . self::API_VERSION,
+                ]),
+            ],
+        ];
+
+        $context = stream_context_create($options);
+        $content = file_get_contents($url, false, $context);
+        return $content;
+    }
+
+    private function saveFile($name, $content)
+    {
+        file_put_contents($name, $content);
+    }
 }
-
-// Esempio di utilizzo
-$repoOwner = "buggyzap";
-$repoName = "ector";
-$accessToken = "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";
-
-$downloader = new Downloader($repoOwner, $repoName, $accessToken);
-$downloader->downloadLatestReleaseAssets();
