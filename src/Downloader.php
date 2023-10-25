6666e976745dcd99119f8c56a11b9ab6f87a61ca
Francesco Mineo <francymin@gmail.com>
2023-10-25 10:58:57 +0200

    refactor(helperclass): created DownloaderHelper class

--- src/Downloader.php
+++ src/Downloader.php
@@ -2,14 +2,14 @@
 
 namespace Ector\ReleaseDownloader;
 
+use Ector\ReleaseDownloader\Helper\DownloaderHelper;
+
 class Downloader
 {
     const GITHUB_API_URL = "https://api.github.com/";
     const GITHUB_API_VERSION = "2022-11-28";
     const USER_AGENT = "EctorReleaseDownloader";
-    const BACKEND_API_URL = "http://192.168.1.49:1337/api/";
-    const BACKEND_API_TOKEN = "15745c4feb3004cb96eb6f5b86327e809453ae8249a9a2c6502a5e5547a252e79048c8ea6ec337e4789a7cc9ab3ccae70872b4303930edcc694aff8db59ee1e15997c642047c06a2bf0d2ec412538cdc840706ccc7807037b95296da6379b0dafb2799e435a51c4c109bb01a20732f500d708d7f59630704a25d98cdf2458c86";
-
+    
     private $repositoryOwner;
     private $repositoryName;
     private $accessToken;
@@ -83,16 +83,6 @@ class Downloader
         return null;
     }
 
-    public function getActiveEctorModules(): array
-    {
-        $ectorModules = [];
-        $activeModules = $this->getActiveEctorModulesApi();
-        foreach ($activeModules as $module) {
-            $ectorModules[$module['attributes']['name']] = $module['attributes']['version'];
-        }
-        return $ectorModules;
-    }
-
     /**
      * Private Methods
      */
@@ -115,7 +105,7 @@ class Downloader
             ],
         ];
 
-        $releaseInfo = $this->performCurlRequest($url, $options);
+        $releaseInfo = DownloaderHelper::performCurlRequest($url, $options);
 
         if ($releaseInfo) {
             return json_decode($releaseInfo, true);
@@ -156,7 +146,7 @@ class Downloader
             ],
         ];
 
-        return $this->performCurlRequest($url, $options);
+        return DownloaderHelper::performCurlRequest($url, $options);
     }
 
     private function downloadFile($url)
@@ -170,44 +160,14 @@ class Downloader
             ],
         ];
 
-        return $this->performCurlRequest($url, $options);
+        return DownloaderHelper::performCurlRequest($url, $options);
     }
 
-    private function performCurlRequest($url, $options)
-    {
-        $ch = curl_init();
-        curl_setopt_array($ch, $options);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        return $result;
-    }
+    
 
     private function saveFile($name, $content)
     {
         file_put_contents(_PS_MODULE_DIR_ . $name, $content);
     }
 
-    private function getActiveEctorModulesApi(): array
-    {
-
-        $url = self::BACKEND_API_URL . "modules?filters[active][%24eq]=true";
-
-        $options = [
-            CURLOPT_HTTPHEADER => [
-                'Accept: application/json',
-                'Authorization: Bearer ' . self::BACKEND_API_TOKEN,
-            ],
-        ];
-
-        $response = json_decode($this->performCurlRequest($url, $options), true);
-
-        return $response['data'];
-    }
 }
